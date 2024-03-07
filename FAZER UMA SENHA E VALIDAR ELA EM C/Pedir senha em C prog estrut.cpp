79f6cd2723a5ce11ed29dc416c8558f4fe84f42f
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-03-07 16:52:28 -0300

    Enviando arquivo

---
+++ FAZER UMA SENHA E VALIDAR ELA EM C/Pedir senha em C prog estrut.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+	int contador;
+	int senhadigitada;
+	int senhacadastrada;
+	printf("Faça uma senha: \n");
+	scanf("%d", &senhacadastrada);
+	while (contador<3){
+	printf("Digite sua senha de cadastro \n");
+	scanf("%d", &senhadigitada);
+	if(senhacadastrada==senhadigitada){	
+	printf("senha correta \n");
+	break;
+	}else if(contador>3){
+	printf("continue tentando \n");
+	}	
+	contador ++;
+	}
+   if (contador == 3){
+ 	printf("Cadastro bloqueado");
+ }	
+	return 0;
+}
